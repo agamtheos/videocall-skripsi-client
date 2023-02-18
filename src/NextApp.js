f17b8f4f7457827d7b55d61399e030b01e7578aa
Agam Theos <agamtheos@gmail.com>
2023-02-19 01:54:12 +0700

    refactor: import less

--- src/NextApp.js
+++ src/NextApp.js
@@ -1,40 +1,12 @@
-// import { useMemo, useState } from "react";
-// import LoginForm from "./components/login/loginForm";
-// import VideoCallPage from "./components/videoCallPage/videoCallPage";
-// import { AuthProvider } from "./context/authContext";
-
-// function App() {
-//   const [authData, setAuthData] = useState({username: "", token: "", isAuthenticated: false});
-
-//   const AuthDataValue = useMemo(() => {
-//     return authData
-//   }, [authData]);
-
-//   const changeAuthDataValue = (newVal) => {
-//     setAuthData((prevVal) => {
-//       return{...prevVal, ...newVal};
-//     })
-//   }
-
-//   return (
-//     <AuthProvider value={{AuthDataValue, changeAuthDataValue}}>
-//     <div className="page">
-//       <LoginForm /> 
-//     </div>
-//     </AuthProvider>
-//   );
-// }
-
-// export default App;
-
-
 import React from "react";
 import {Provider} from 'react-redux'
 import {ConnectedRouter} from 'connected-react-router'
 import {Route, Switch} from "react-router-dom";
 
-import store, {history} from './appRedux/store';
-import App from './routes/index';
+import store, {history} from '@store'
+import App from '@routes'
+import "assets/vendors/style";
+import "styles/wieldy.less";
 
 const NextApp = () =>
   <Provider store={store}>
