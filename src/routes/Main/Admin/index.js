94d3a3ad86c5ec2088ad7c62c263d7ae2f87db2f
tubagusputra <tubagusputranovaldi@gmail.com>
2023-05-20 14:08:01 +0700

    feat: add room & video call route

--- src/routes/Main/Admin/index.js
+++ src/routes/Main/Admin/index.js
@@ -1,14 +1,29 @@
 import React, { memo, Suspense, lazy } from "react";
 import { Route, Switch } from "react-router-dom";
+
 import Fallback from "../../../components/Fallback";
 
-export default memo(({match}) => {
+export default memo(({ match }) => {
     return (
-    <Suspense fallback={<Fallback />}>
-        <Switch>
-            <Route path={`${match.url}/admin`} component={lazy(() => import("./CallPage"))} />
-            <Route path={`${match.url}/user`} component={lazy(() => import("./HomePage"))} />
-        </Switch>
-    </Suspense>
-    )
+        <Suspense fallback={<Fallback />}>
+            <Switch>
+                <Route
+                    path={`${match.url}/admin`}
+                    component={lazy(() => import("./CallPage"))}
+                />
+                <Route
+                    path={`${match.url}/user`}
+                    component={lazy(() => import("./HomePage"))}
+                />
+                <Route
+                    path={`${match.url}/test1`}
+                    component={lazy(() => import("./RoomPage"))}
+                />
+                <Route
+                    path={`${match.url}/test2`}
+                    component={lazy(() => import("./VideoCallPage"))}
+                />
+            </Switch>
+        </Suspense>
+    );
 });
