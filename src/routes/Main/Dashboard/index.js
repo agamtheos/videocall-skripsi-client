dd799a5cb254699db8ae988bc79cfc4d22898697
Agam Theos <agamtheos@gmail.com>
2023-06-04 18:45:20 +0700

    feat: add acces list routes

--- src/routes/Main/Dashboard/index.js
+++ src/routes/Main/Dashboard/index.js
@@ -1,26 +1,31 @@
 import React, { memo, Suspense, lazy } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
+import { getDefaultRoute, roleable, DASHBOARD_ADMIN, DASHBOARD_CLIENT } from "../../../util/features";
 import Fallback from "../../../components/Fallback";
 
 export default memo((props) => {
     const { match } = props;
+    const defaultRoute = getDefaultRoute();
 
     return (
             <Suspense fallback={<Fallback />}>
             <Switch>
-                <Route
+                {roleable(DASHBOARD_ADMIN) && <Route path={`${match.url}/admin`} component={lazy(() => import("./Admin"))} />}
+                {roleable(DASHBOARD_CLIENT) && <Route path={`${match.url}/client`} component={lazy(() => import("./Client"))} />}
+                {/* <Route
                     path={`${match.url}/admin`}
                     component={lazy(() => import("./Admin"))}
-                />
-                <Route
+                /> */}
+                {/* <Route
                     path={`${match.url}/client`}
                     component={lazy(() => import("./Client"))}
-                />
+                /> */}
                 <Route
                     path={`${match.url}/profile`}
                     component={lazy(() => import("./Profile"))}
                 />
+                {!!defaultRoute && <Redirect to={defaultRoute}/>}
                 <Route
                     path={`${match.url}/test1`}
                     component={lazy(() => import("./RoomPage"))}
--- src/util/features.js
+++ src/util/features.js
@@ -3,14 +3,30 @@ import React from 'react';
 export const ROLE_ADMIN = 'admin';
 export const ROLE_CLIENT = 'client';
 
+// page permissions
+export const DASHBOARD_ADMIN = 'DASHBOARD_ADMIN';
+export const MANAGE_USER = 'MANAGE_USER';
+export const DASHBOARD_CLIENT = 'DASHBOARD_CLIENT';
+
+const adminAccessList = [
+    DASHBOARD_ADMIN,
+    MANAGE_USER,
+]
+
+const clientAccessList = [
+    DASHBOARD_CLIENT,
+]
+
 const PERMISSIONS = [
     {
         role: ROLE_ADMIN,
         defaultRoute: '/dashboard/admin/home',
+        features: adminAccessList,
     },
     {
         role: ROLE_CLIENT,
         defaultRoute: '/dashboard/client/home',
+        features: clientAccessList,
     }
 ]
 
@@ -33,3 +49,9 @@ export const RoleableComponent = ({children}) => (
 		{getDefaultRoute() ? children : null}
 	</React.Fragment>
 );
+
+export const roleable = (featureCode) => {
+	const activeRole = getActiveRole();
+	const {features} = PERMISSIONS.find(({role}) => role === activeRole) ?? {};
+	return features ? features.includes(featureCode) : false;
+}
