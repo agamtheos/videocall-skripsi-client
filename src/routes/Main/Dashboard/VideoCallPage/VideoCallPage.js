a68955d3d2ec05e11ee8670b118cb3d899378fec
Agam Theos <agamtheos@gmail.com>
2023-06-21 22:09:36 +0700

    debug: disable video

--- src/routes/Main/Dashboard/VideoCallPage/VideoCallPage.js
+++ src/routes/Main/Dashboard/VideoCallPage/VideoCallPage.js
@@ -70,9 +70,9 @@ const VideoCallPage = () => {
             console.log(stream)
             return [stream];
         }
-        stream.forEach((item) => {
-            item.getVideoTracks()[0].enabled = !(item.getVideoTracks()[0].enabled)
-        })
+        // stream.forEach((item) => {
+        //     item.getVideoTracks()[0].enabled = !(item.getVideoTracks()[0].enabled)
+        // })
         return stream;
     };
 
