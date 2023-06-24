0678e2d4e2b214c440b1137cdfe115500cd05321
Agam Theos <agamtheos@gmail.com>
2023-06-24 20:25:27 +0700

    fix: black screen on safari and ios

--- src/classes/Connection.js
+++ src/classes/Connection.js
@@ -8,7 +8,6 @@ const WebRtcPeerClass = require('./WebRtcPeer');
 const WebRtcPeer = new WebRtcPeerClass()
 
 export const callResponse = (message) => {
-    console.log('RENEEEJEJEJEJ')
     if (message.response === 'rejected') {
         Modal.error({
             title: "Panggilan Ditolak",
@@ -42,23 +41,6 @@ export const callResponse = (message) => {
             },
         })
     }
-        // set call state = in call
-        // const webRtcPeer = WebRtcPeer.getPeers()
-        // console.log(webRtcPeer)
-        // webRtcPeer.onicecandidate = function (event) {
-        //     if (event.candidate) {
-        //         const msg = {
-        //             id : 'onIceCandidate',
-        //             candidate : event.candidate,
-        //             to : message.from,
-        //             from: message.to
-        //         }
-        //         sendMessage(msg);
-        //     }
-        // } 
-        // webRtcPeer.processAnswer(message.sdpAnswer, function (error) {
-        //     if (error) return console.error(error);
-        // });
 }
 
 export const registerResponse = async (message) => {
@@ -69,7 +51,6 @@ export const registerResponse = async (message) => {
         var errorMessage = message.message ? message.message
                 : 'Unknown reason for register rejection.';
         console.log(errorMessage);
-        // alert('Error registering user. See console for further information.');
     }
 }
 
@@ -81,101 +62,6 @@ export const call = async (from, to) => {
         state: 'REQ_CALLING'
     }
     sendMessage(message);
-
-    // let configuration = {
-    //     iceServers: [
-    //         {
-    //             urls: "stun:stun1.l.google.com:19302"
-    //         },
-    //         {
-    //             urls: "turn:a.relay.metered.ca:80",
-    //             username: "417e29407130059049b7c92e",
-    //             credential: "4CZ5bkgLqE0QjdRU",
-    //         },
-    //         {
-    //             urls: "turn:a.relay.metered.ca:80?transport=tcp",
-    //             username: "417e29407130059049b7c92e",
-    //             credential: "4CZ5bkgLqE0QjdRU",
-    //         },
-    //         {
-    //             urls: "turn:a.relay.metered.ca:443",
-    //             username: "417e29407130059049b7c92e",
-    //             credential: "4CZ5bkgLqE0QjdRU",
-    //         },
-    //         {
-    //             urls: "turn:a.relay.metered.ca:443?transport=tcp",
-    //             username: "417e29407130059049b7c92e",
-    //             credential: "4CZ5bkgLqE0QjdRU",
-    //         },
-    //     ],
-    // };
-
-    // const peer = new RTCPeerConnection(configuration);
-    // // const peer = new RTCPeerConnection();
-
-    // const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true })
-    // // peer.addTrack(stream.getTracks()[0], stream)
-    // await peer.addStream(stream)
-
-    // // create offer
-    // const offer = await peer.createOffer();
-
-    // let message = {
-    //     id : 'call',
-    //     from : from, // user yang melakukan panggilan
-    //     to : to, // user yang menerima panggilan
-    //     sdpOffer : offer,
-    //     state: 'req_calling'
-    // };
-    // sendMessage(message);
-    
-    // await peer.setLocalDescription(offer);
-    // WebRtcPeer.addPeer(peer)
-
-    // // set delay 1s
-    
-    // peer.onicecandidate = function (event) {
-    //     if (event.candidate) {
-    //         const message = {
-    //             id : 'onIceCandidate',
-    //             candidate : event.candidate,
-    //             to : to,
-    //             from: from
-    //         }
-    //         sendMessage(message);
-    //     }
-    // }
-
-    // // get to know when connected to peer
-    // peer.onconnectionstatechange = function (event) {
-    //     console.log('masuk sono')
-    //     if (peer.connectionState === 'connected') {
-    //         const message = {
-    //             id: 'peerConnected',
-    //             from: localStorage.getItem('me'),
-    //             to: localStorage.getItem('they')
-    //         }
-    //         sendMessage(message)
-    //     }
-
-    //     if (peer.connectionState === 'disconnected' || peer.connectionState === 'failed' || peer.connectionState === 'closed') {
-    //         peer.close();
-    //         console.log('Connection closed, because of ' + peer.connectionState);
-    //     }
-    // }
-
-    // // peer.onicegatheringstatechange = function (event) {
-    // //     console.log('masuk sono')
-    // //     if (peer.iceGatheringState === 'complete') {
-    // //         const message = {
-    // //             id: 'peerConnected',
-    // //             from: localStorage.getItem('me'),
-    // //             to: localStorage.getItem('they')
-    // //         }
-    // //         sendMessage(message)
-    // //     }
-    // // }
-
 }
 
 export const register = (name) => {
@@ -189,108 +75,13 @@ export const register = (name) => {
 }
 
 export const incomingCall = async (message) => {
-    const peer = WebRtcPeer.getPeers()
-    const  stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+    // const peer = WebRtcPeer.getPeers()
+    // const  stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true })
 
-    stream.getTracks().forEach(track => {
-        // add all tracks to peer
-        peer.addTrack(track, stream)
-    })
-
-    // const configuration = {
-    //     iceServers: [
-    //         {
-    //             urls: "stun:stun1.l.google.com:19302"
-    //         },
-    //         {
-    //             urls: "turn:a.relay.metered.ca:80",
-    //             username: "417e29407130059049b7c92e",
-    //             credential: "4CZ5bkgLqE0QjdRU",
-    //         },
-    //         {
-    //             urls: "turn:a.relay.metered.ca:80?transport=tcp",
-    //             username: "417e29407130059049b7c92e",
-    //             credential: "4CZ5bkgLqE0QjdRU",
-    //         },
-    //         {
-    //             urls: "turn:a.relay.metered.ca:443",
-    //             username: "417e29407130059049b7c92e",
-    //             credential: "4CZ5bkgLqE0QjdRU",
-    //         },
-    //         {
-    //             urls: "turn:a.relay.metered.ca:443?transport=tcp",
-    //             username: "417e29407130059049b7c92e",
-    //             credential: "4CZ5bkgLqE0QjdRU",
-    //         },
-    //     ],
-    // }
-
-    // // create peer using RTC
-    // const peer = new RTCPeerConnection(configuration);
-    // // const peer = new RTCPeerConnection();
-
-    // peer.setRemoteDescription(new RTCSessionDescription(message.sdpOffer))
-
-    // const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true })
-    // // peer.addTrack(stream.getTracks()[0], stream)
-    // await peer.addStream(stream)
-
-    // const answer = await peer.createAnswer();
-
-    // await peer.setLocalDescription(answer);
-
-    // WebRtcPeer.addPeer(peer)
-
-    // peer.onicecandidate = function (event) {
-    //     if (event.candidate) {
-    //         const msg = {
-    //             id : 'onIceCandidate',
-    //             candidate : event.candidate,
-    //             to : message.from,
-    //             from: message.to
-    //         }
-    //         sendMessage(msg);
-    //     }
-    // }
-
-    // // get to know when connected to peer
-    // peer.onconnectionstatechange = function (event) {
-    //     console.log('masuk sono')
-    //     if (peer.connectionState === 'connected') {
-    //         const message = {
-    //             id: 'peerConnected',
-    //             from: localStorage.getItem('me'),
-    //             to: localStorage.getItem('they')
-    //         }
-    //         sendMessage(message)
-    //     }
-
-    //     if (peer.connectionState === 'disconnected' || peer.connectionState === 'failed' || peer.connectionState === 'closed') {
-    //         peer.close();
-    //         console.log('Connection closed, because of ' + peer.connectionState);
-    //     }
-    // }
-
-    // // peer.onicegatheringstatechange = function (event) {
-    // //     console.log('masuk sono')
-    // //     if (peer.iceGatheringState === 'complete') {
-    // //         const message = {
-    // //             id: 'peerConnected',
-    // //             from: localStorage.getItem('me'),
-    // //             to: localStorage.getItem('they')
-    // //         }
-    // //         sendMessage(message)
-    // //     }
-    // // }
-
-    // let response = {
-    //     id: 'incomingCallResponse',
-    //     from: message.from,
-    //     callResponse: 'accept',
-    //     sdpOffer: answer,
-    //     state: 'acc_calling'
-    // }
-    // sendMessage(response);
+    // stream.getTracks().forEach(track => {
+    //     // add all tracks to peer
+    //     peer.addTrack(track, stream)
+    // })
     localStorage.setItem('they', message.from)
 }
 
